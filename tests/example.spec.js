995577ea60c5cbc1f75186ec98740807372558d1
icecam0001 <kupermancameron@gmail.com>
2025-04-24 20:08:10 -0700

    wait it out

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,9 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, expect } = require('@playwright/test')
 
-describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('/')
-    await expect(page.getByText('ivysaur')).toBeVisible()
-    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
-  })
+test('front page can be opened', async ({ page }) => {
+  await page.goto('/');
+  // Add a pause to allow the page to fully load
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('ivysaur')).toBeVisible();
+  await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
 })
