48ce7142b4494e0c7eb3a6338dc166224bd4f02a
icecam0001 <kupermancameron@gmail.com>
2025-04-26 00:50:20 -0400

    makiung a failing test to see if deploymenet still runds

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
   res.send('ok')
-})
-app.listen(PORT, () => {
+});;
+app.listen(PORT, () => { ;
   console.log(`server started on port ${PORT}`)
-})
+});
