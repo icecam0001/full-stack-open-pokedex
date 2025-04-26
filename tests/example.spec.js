a6c8bb4c068a22b52058e8680246c2d3b335d5f9
icecam0001 <kupermancameron@gmail.com>
2025-04-26 00:37:22 -0400

    fixing it

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test')
 
 test('front page can be opened', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/')
   // Add a pause to allow the page to fully load
-  await page.waitForTimeout(3000);
-  await expect(page.getByText('ivysaur')).toBeVisible();
-  await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
+  await page.waitForTimeout(3000)
+  await expect(page.getByText('ivysaur')).toBeVisible()
+  await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
 })
